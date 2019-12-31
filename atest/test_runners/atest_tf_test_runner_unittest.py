0213bfcfffc47d9c065767eaaa60c943dfce5836
Jim Tang <jimtang@google.com>
2019-12-31 11:37:10 +0800

    Atest: resolve import error in unit tests.
    Bug: 146810748
    
    Test: ./run_atest_unittests.sh
      without seeing import error.
    
    Change-Id: I154483f003657996102aec04f4696ac8c2ca1983

--- atest/test_runners/atest_tf_test_runner_unittest.py
+++ atest/test_runners/atest_tf_test_runner_unittest.py
@@ -30,9 +30,9 @@ from unittest import mock
 import constants
 import unittest_constants as uc
 import unittest_utils
-import event_handler
 
 from test_finders import test_info
+from test_runners import event_handler
 from test_runners import atest_tf_test_runner as atf_tr
 
 #pylint: disable=protected-access
