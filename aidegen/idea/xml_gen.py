7a64e3d1e7af1093a8ac3c49fa959081485c5525
patricktu <patricktu@google.com>
2020-05-22 19:40:01 +0800

    AIDEGen: Set the ignored Git paths in the workspace.xml
    Bug: 154436905
    Test: 1. sh run_tests.sh;atest aidegen_unittests --host
             Keeps the code coverage 100% of the changes.
          2. m aidegen;aidegen-dev Settings
             Check the .idea/vcs.xml has only the Git path of Settings.
          3. Under AOSP root folder:
             a. aidegen-dev
                Check the .idea/workspace.xml has the ignored Git paths.
             b. Manually add packages/apps/Settings to vcs.xml
             c. aidegen-dev
                Check the packages/apps/Settings in the vcs.xml.
                Check the workspace.xml has the ignored Git paths, including
    	    the Settings.
    Change-Id: Iedcda458a8248394133430cdb762f792a67b86c7

--- aidegen/idea/xml_gen.py
+++ aidegen/idea/xml_gen.py
@@ -35,12 +35,16 @@ from __future__ import absolute_import
 
 import os
 
+from xml.etree import ElementTree
+
 from aidegen import constant
 from aidegen import templates
+from aidegen.lib import aidegen_metrics
 from aidegen.lib import common_util
 from aidegen.lib import xml_util
 
 _GIT_PATH = '        <mapping directory="{GIT_DIR}" vcs="Git" />'
+_IGNORE_PATH = '            <path value="{GIT_DIR}" />'
 
 
 class XMLGenerator:
@@ -91,6 +95,21 @@ class XMLGenerator:
         return [e for e in self._xml_obj.findall(element_type)
                 if e.get('name') == name]
 
+    @staticmethod
+    def append_node(parent, node_str):
+        """Appends a node string under the parent element.
+
+        Args:
+            parent: An element object, the new node's parent.
+            node_str: A string, the new node's content.
+        """
+        try:
+            parent.append(ElementTree.fromstring(node_str))
+        except ElementTree.ParseError as xml_err:
+            aidegen_metrics.send_exception_metrics(
+                exit_code=constant.XML_PARSING_FAILURE, stack_trace=xml_err,
+                log=node_str, err_msg='')
+
     def create_xml(self):
         """Creates the xml file."""
         common_util.file_generate(self._xml_abspath, common_util.to_pretty_xml(
@@ -105,7 +124,7 @@ def gen_vcs_xml(module_path, git_paths):
     ignored Git paths in the workspace.xml.
 
     Args:
-        module_path: Path of the module.
+        module_path: A string, the absolute path of the module.
         git_paths: A list of git paths.
     """
     git_mappings = [_GIT_PATH.format(GIT_DIR=p) for p in git_paths]
@@ -113,3 +132,25 @@ def gen_vcs_xml(module_path, git_paths):
     if module_path != common_util.get_android_root_dir() or not vcs.xml_obj:
         common_util.file_generate(vcs.xml_path, templates.XML_VCS.format(
             GIT_MAPPINGS='\n'.join(git_mappings)))
+
+
+def write_ignore_git_dirs_file(module_path, ignore_paths):
+    """Write the ignored git paths in the .idea/workspace.xml.
+
+    Args:
+        module_path: A string, the absolute path of the module.
+        ignore_paths: A list of git paths.
+    """
+    ignores = [_IGNORE_PATH.format(GIT_DIR=p) for p in ignore_paths]
+    workspace = XMLGenerator(module_path, 'workspace.xml')
+    if not workspace.xml_obj:
+        common_util.file_generate(workspace.xml_path,
+                                  templates.XML_WORKSPACE.format(
+                                      GITS=''.join(ignores)))
+        return
+    for conf in workspace.find_elements_by_name('component',
+                                                'VcsManagerConfiguration'):
+        workspace.xml_obj.getroot().remove(conf)
+    workspace.append_node(workspace.xml_obj.getroot(),
+                          templates.IGNORED_GITS.format(GITS=''.join(ignores)))
+    workspace.create_xml()
