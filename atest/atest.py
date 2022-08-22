c61c396b5a61822cebb79fd43c9711fa96e32617
Jim Tang <jimtang@google.com>
2022-08-22 23:33:58 +0800

    Fix need_update_merged_file logic error
    This change fixes the logic error `need_update_merged_file` which
    mistakely compares two pairs of checksums that came from the same method
    get_module_info_checksum, and results in no chance to merge these
    essential json files.
    
    Test: atest-src hello_world_test --host
    Test: echo >> tools/asuite/atest/Android.bp
    Test: atest-src hello_world_test --host
    
    Bug: 243380795
    Change-Id: I9b326511ac07d30160c6a32729ca43caa4ebcbab

--- atest/atest.py
+++ atest/atest.py
@@ -967,11 +967,7 @@ def main(argv, results_dir, args):
     mod_stop = time.time() - mod_start
     metrics.LocalDetectEvent(detect_type=DetectType.MODULE_INFO_INIT_MS,
                              result=int(mod_stop * 1000))
-    atest_utils.run_multi_proc(
-        func=mod_info._save_module_info_checksum,
-        args=[[mod_info.mod_info_file_path,
-               mod_info.java_dep_path,
-               mod_info.cc_dep_path]])
+    atest_utils.run_multi_proc(func=mod_info._save_module_info_checksum)
     atest_utils.run_multi_proc(
         func=atest_utils.generate_buildfiles_checksum,
         args=[mod_info.module_index.parent])
