eb2e35efddd5468a7b21f1cddd36d69e5e229eda
Luca Farsi <lucafarsi@google.com>
2023-03-27 16:59:03 -0700

    Skip irrelevant BEP events in BazelTest
    When BazelTest was refactored to use the BepFileTailer to read events
    from the BEP file, the logic to skip non TestResult events that we don't
    care about wasn't included which causes BazelTest to report failures.
    Re-add that logic.
    
    Test: atest --host bazel-test-runner-tests
    Bug: 275421090
    Change-Id: Iba6a9535e420a32ee5868871207cc0cdbd66af58

--- atest/bazel/runner/src/com/android/tradefed/testtype/bazel/BazelTest.java
+++ atest/bazel/runner/src/com/android/tradefed/testtype/bazel/BazelTest.java
@@ -54,7 +54,6 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -62,10 +61,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.concurrent.Future;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.zip.ZipFile;
 
@@ -228,6 +227,10 @@ public final class BazelTest implements IRemoteTest {
         while ((event = tailer.nextEvent()) != null) {
             if (event.getLastMessage()) {
                 return;
+            }
+
+            if (!event.hasTestResult()) {
+                continue;
             }
 
             try {
