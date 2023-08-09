c91a5771428852809430cd19372a2860891837e1
Ronald Braunstein <rbraunstein@google.com>
2023-08-09 16:22:06 -0700

    Add restart command to update.
    Added adb commands to restart/reboot the device as appropriate
    based on installed_files in module-info.json.
    
    Removed `sync` subcommand as it isn't safe without doing a `adb exec-out stop` and `adb exec-out start`.
    
    Notes about the commit:
    
    adevice.rs: Mainly removed sync and added Update. I added rust logger
    with log levels. I want to play with it more and see if I can do
    clearcut logging with it.
      For now, do `adevice update -d` for  "debug" logging and
       `adevice update -v` for info logging, and `adevice update` for
       a quieter sequence.
      Much work still needs to be done here, along with adding "/" to
      the file paths.
    
    Added:
    
    device.rs:
      The restart and wait adb sequences are implemented here.
    
    restart_chooser.rs:
      The functionality is under `RestartChooser::restart_type` to determine
      if we should reboot or "exec start" for the given installed files.
      The algorithm looks up the "Class" in module_info for the installed
      files and chooses the restart type based on the class.
    
    Test: atest
    Test: adevice update -d
    Change-Id: Ic297520f1534591a48e15a65e20e75dbd481f5f3

--- adevice/src/cli.rs
+++ adevice/src/cli.rs
@@ -17,31 +17,31 @@ pub enum Commands {
     /// Show the actions that would be run.
     ShowActions,
     /// Update the device
-    Sync {
-        /// Do not run any commands, just print them
-        #[clap(long = "dry-run", short = 'n', default_value_t = false)]
-        dryrun: bool,
-        /// Print commands while executing them.
-        #[clap(long = "verbose", short = 'v', default_value_t = true)]
-        verbose: bool,
-        /// Do not make any modification if more than this many are needed
-        #[clap(long, short, default_value_t = 20)]
-        max_allowed_changes: usize,
-    },
+    Update,
 }
 
 #[derive(Debug, Args)]
 pub struct GlobalOptions {
     // TODO(rbraunstein): Revisit all the command name descriptions.
+    // TODO(rbraunstein): Add system_other to the default list, but deal gracefully
+    // with it not being on the device.
     /// Partitions in the product tree to sync. Repeat arg or comma-separate.
     #[clap(long, short, global = true,
-    default_values_t = [String::from("system")], value_delimiter = ',')]
+    default_values_t = [String::from("system"), String::from("system_ext")], value_delimiter = ',')]
     pub partitions: Vec<String>,
     // TODO(rbraunstein): Validate relative, not absolute paths.
     #[clap(global = true)]
     /// If unset defaults to ANDROID_PRODUCT_OUT env variable.
     pub product_out: Option<String>,
-    // Path of file to write out with the status update or dump of device or build tree
-    // hashes.
-    pub dump_file_prefix: Option<String>,
+    /// Do not make any modification if more than this many are needed
+    #[clap(long, short, default_value_t = 20)]
+    pub max_allowed_changes: usize,
+    // TODO(rbraunstein): Import clap-verbosity-flag crate so we can use -vv instead
+    // of having two flags. Or add an output level enum.
+    /// Print commands while executing them.
+    #[clap(long = "verbose", short, default_value_t = false, global = true)]
+    pub verbose: bool,
+    /// Print commands while executing them.
+    #[clap(long = "debug", short, default_value_t = false, global = true)]
+    pub debug: bool,
 }
