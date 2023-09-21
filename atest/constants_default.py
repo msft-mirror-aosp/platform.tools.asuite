975cac3f44b4392333eadcfbb8042e70f76c8e90
Nelson Li <nelsonli@google.com>
2023-09-21 06:56:14 +0000

    Remove --serial flag from ROBOLEAF_UNSUPPORTED_FLAGS
    This change removes the --serial flag from the
    ROBOLEAF_UNSUPPORTED_FLAGS. The modification is made based on the
    observation that the --serial option now functions correctly within
    RoboleafTestRunner, making it unnecessary to include in the list of
    unsupported flags.
    
    Bug: 301187663
    Test: m atest && atest-dev adbd_test --roboleaf-mode=dev --serial xxx
    Test: m atest && atest-dev --host atest_unittests
    Change-Id: If14e9b3e267fd872cc77c8e798fe697e4fbcc07b

--- atest/constants_default.py
+++ atest/constants_default.py
@@ -411,7 +411,7 @@ SHARD_NUM = 2
 
 # Flags which roboleaf mode already supported:
 #   --iterations, --rerun-until-failure, --retry-any-failure, --verbose,
-#   --bazel-arg, --, --wait-for-debugger, --host
+#   --bazel-arg, --, --wait-for-debugger, --host, --serial
 #
 # Flags which roboleaf mode doesn't need to support:
 #   --minimal-build, --bazel_mode, --null-feature, --experimental-remote-avd,
@@ -463,7 +463,6 @@ ROBOLEAF_UNSUPPORTED_FLAGS = {
     'fuzzy_search': UnsupportedFlag(lambda d, v: d != v, ""),
     'acloud_create': UnsupportedFlag(lambda d, v: d != v, ""),
     'start_avd': UnsupportedFlag(lambda d, v: d != v, ""),
-    'serial': UnsupportedFlag(lambda d, v: d != v, ""),
     'flakes_info': UnsupportedFlag(lambda d, v: d != v, ""),
     'tf_early_device_release': UnsupportedFlag(lambda d, v: d != v, ""),
     'test_config_select': UnsupportedFlag(lambda d, v: d != v, ""),
