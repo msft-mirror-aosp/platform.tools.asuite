ec528396c44e519b48ac2e5b5035e822a0438baa
yike <yikezh@google.com>
2024-01-12 23:33:41 +0000

    Support `--serial` in device update
    When there are more than one connected device, users need to specify
    the device to update.
    
    Test: atest-dev hello_world_test -s 127.0.0.1:35363
    Bug: 319888373
    Change-Id: Id26043bce16199929ef0ba70cfe7da00add4ecae

--- atest/device_update.py
+++ atest/device_update.py
@@ -20,17 +20,21 @@ from pathlib import Path
 from subprocess import CalledProcessError
 
 from abc import ABC, abstractmethod
-from typing import Set
+from typing import List, Set
 
 from atest import atest_utils
+from atest import constants
 
 
 class DeviceUpdateMethod(ABC):
     """A device update method used to update device."""
 
     @abstractmethod
-    def update(self):
+    def update(self, serials: List[str]=None):
         """Updates the device.
+
+        Args:
+            serials: A list of serial numbers.
 
         Raises:
             Error: If the device update fails.
@@ -42,7 +46,7 @@ class DeviceUpdateMethod(ABC):
 
 
 class NoopUpdateMethod(DeviceUpdateMethod):
-    def update(self) -> None:
+    def update(self, serials: List[str]=None) -> None:
         pass
 
     def dependencies(self) -> Set[str]:
@@ -55,12 +59,24 @@ class AdeviceUpdateMethod(DeviceUpdateMethod):
     def __init__(self, adevice_path: Path = _TOOL):
         self._adevice_path = adevice_path
 
-    def update(self) -> None:
+    def update(self, serials: List[str]=None) -> None:
         try:
             print(atest_utils.mark_cyan("\nUpdating device..."))
             update_start = time.time()
 
-            atest_utils.run_limited_output([self._adevice_path, 'update'])
+            update_cmd = [self._adevice_path, 'update']
+            if serials:
+                if len(serials) > 1:
+                    atest_utils.colorful_print(
+                        'Warning: Device update feature can only update one '
+                        'device for now, but this invocation specifies more '
+                        'than one device. Atest will update the first device '
+                        'by default.',
+                        constants.YELLOW)
+
+                update_cmd.extend(['--serial', serials[0]])
+
+            atest_utils.run_limited_output(update_cmd)
 
             print(atest_utils.mark_cyan(
                 '\nDevice update finished in '
