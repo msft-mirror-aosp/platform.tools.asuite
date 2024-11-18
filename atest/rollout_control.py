7d891025d9635a3a433a4ae67b7c577975ef95fc
Yuexi Ma <yuexima@google.com>
2024-11-18 14:56:35 -0800

    Support gradual feature rollout
    Test: atest unittests
    Fix: 379742333
    Change-Id: Idacec2c7d422faa42a9897cf0f67b0a741c840d8

---
+++ atest/rollout_control.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# Copyright 2024, The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Rollout control for Atest features."""
+
+import getpass
+import hashlib
+import logging
+import os
+
+
+class RolloutControlledFeature:
+  """Base class for Atest features under rollout control."""
+
+  def __init__(
+      self, name: str, rollout_percentage: float, env_control_flag: str
+  ):
+    """Initializes the object.
+
+    Args:
+        name: The name of the feature.
+        rollout_percentage: The percentage of users to enable the feature for.
+          The value should be in [0, 100].
+        env_control_flag: The environment variable name to override the feature
+          enablement. When set, 'true' or '1' means enable, other values means
+          disable.
+    """
+    self._name = name
+    self._rollout_percentage = rollout_percentage
+    self._env_control_flag = env_control_flag
+
+  def _check_env_control_flag(self) -> bool | None:
+    """Checks the environment variable to override the feature enablement.
+
+    Returns:
+        True if the feature is enabled, False if disabled, None if not set.
+    """
+    if self._env_control_flag not in os.environ:
+      return None
+    return os.environ[self._env_control_flag] in ('TRUE', 'True', 'true', '1')
+
+  def is_enabled(self, username: str | None = None) -> bool:
+    """Checks whether the current feature is enabled for the user.
+
+    Args:
+        username: The username to check the feature enablement for. If not
+          provided, the current user's username will be used.
+
+    Returns:
+        True if the feature is enabled for the user, False otherwise.
+    """
+    override_flag_value = self._check_env_control_flag()
+    if override_flag_value is not None:
+      logging.debug(
+          'Feature %s is %s by env variable %s.',
+          self._name,
+          'enabled' if override_flag_value else 'disabled',
+          self._env_control_flag,
+      )
+      return override_flag_value
+
+    if username is None:
+      username = getpass.getuser()
+
+    if not username:
+      logging.error(
+          'Unable to determine the username. Disabling the feature %s.',
+          self._name,
+      )
+      return False
+
+    hash_object = hashlib.sha256()
+    hash_object.update((username + ' ' + self._name).encode('utf-8'))
+
+    is_enabled = (
+        int(hash_object.hexdigest(), 16) % 100 < self._rollout_percentage
+    )
+
+    logging.debug(
+        'Feature %s is %s for user %s.',
+        self._name,
+        'enabled' if is_enabled else 'disabled',
+        username,
+    )
+
+    return is_enabled
