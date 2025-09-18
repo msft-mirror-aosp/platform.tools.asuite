57db867ca7a5aa042ba2549f6ca4554e4358d0d3
Yuexi Ma <yuexima@google.com>
2025-09-18 15:30:15 -0700

    Support display CUJ name during perf test multiple config xml selection
    Test: atest --perf ResizeAppWindowPortraitMicrobenchmark --dry-run -c
    Bug: 422232623
    Change-Id: I351752865b730a2ba7fa84161d88b4e8c7688ab7

--- atest/test_finders/test_finder_utils.py
+++ atest/test_finders/test_finder_utils.py
@@ -355,7 +355,9 @@ def extract_selected_tests(
   numbered_list = ['%s: %s' % (i, name_func(t)) for i, t in enumerate(tests)]
   print(
       'Multiple tests found{0}:\n{1}'.format(
-          f' for {target_test_identifier}' if target_test_identifier else '',
+          f' for {atest_utils.mark_cyan(target_test_identifier)}'
+          if target_test_identifier
+          else '',
           '\n'.join(auxiliary_menu + numbered_list),
       )
   )
