66bc42b5ab722ef4556b1200a910da313f07a529
Yuexi Ma <yuexima@google.com>
2025-11-21 14:58:04 -0800

    [Refactor] Use f-string for better readability in create_test_run_dir
    This change replaces the old '%' style string formatting with an f-string in the 'create_test_run_dir' function. F-strings are generally preferred in modern Python for their conciseness and readability, especially when embedding expressions within string literals. This refactoring is purely stylistic and does not alter the existing logic or behavior of the code in any way.
    
    Test: atest atest_unittests
    Flag: TEST_ONLY
    OFF_PEAK_PRESUBMIT: true
    
    Change-Id: I970a75c0e7d24fb6fd27d9900768cdfd68578e32

--- atest/atest_integration_tests.py
+++ atest/atest_integration_tests.py
@@ -139,7 +139,7 @@ def create_test_run_dir():
       A string of the directory path.
   """
   utc_epoch_time = int(time.time())
-  prefix = _TEST_RUN_DIR_PREFIX % utc_epoch_time
+  prefix = f'atest_integration_tests_{utc_epoch_time}_'
   return tempfile.mkdtemp(prefix=prefix)
 
 
