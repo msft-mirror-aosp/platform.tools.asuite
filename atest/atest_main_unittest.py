f3f0aa739d7a5dbdf3e118335abdb142dd40f827
Yuexi Ma <yuexima@google.com>
2025-11-21 15:49:02 -0800

    [Refactor] Improve robustness of a test helper
    The  method in  could raise a  if the supplied  was not in , causing the test to error instead of fail.
    
    This change adds a  check to ensure that  is in  before attempting to find its index. This makes the helper method more robust by providing a clearer failure message instead of an unhandled exception. This change does not alter any of the existing logic.
    
    Test: atest atest_unittests
    Flag: TEST_ONLY
    OFF_PEAK_PRESUBMIT: true
    
    Change-Id: Ic6f843348f399a97917121f671b811eec4b780de

--- atest/atest_main_unittest.py
+++ atest/atest_main_unittest.py
@@ -56,6 +56,7 @@ class AtestUnittests(unittest.TestCase):
     self.assertFalse(atest_main._missing_environment_variables())
 
   def _assert_args_in_order(self, arg_list: List[str], arg0: str, arg1: str):
+    self.assertIn(arg0, arg_list)
     index = arg_list.index(arg0)
     self.assertLess(
         index,
