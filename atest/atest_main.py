30c127a35a531e60e26d33f9335028a71d1360c3
Yuexi Ma <yuexima@google.com>
2025-11-22 10:07:06 -0800

    [Refactor] Use f-string for string formatting in _handle_list_modules
    This change refactors `_handle_list_modules` in `atest_main.py` to use
    f-strings instead of the older `%` formatting. F-strings provide a more
    readable and concise way to format strings in Python 3.
    
    This is a purely stylistic change and does not alter any existing logic
    or functionality.
    
    Test: atest atest_unittests
    Flag: TEST_ONLY
    OFF_PEAK_PRESUBMIT: true
    
    Change-Id: Ia70be17269605eb63f152d2aac6186308123d772

--- atest/atest_main.py
+++ atest/atest_main.py
@@ -1018,15 +1018,15 @@ class _AtestMain:
         self._args.list_modules
     )
     print(
-        '\n%s'
-        % atest_utils.mark_cyan(
-            '%s Testable %s modules'
-            % (len(testable_modules), self._args.list_modules)
+        '\n'
+        + atest_utils.mark_cyan(
+            f'{len(testable_modules)} Testable {self._args.list_modules} '
+            'modules'
         )
     )
     print(atest_utils.delimiter('-'))
     for module in sorted(testable_modules):
-      print('\t%s' % module)
+      print(f'\t{module}')
 
     return ExitCode.SUCCESS
 
