3d7a746fed49025ff1a9d7d84a4197b09f0c3908
Yuexi Ma <yuexima@google.com>
2025-11-24 12:00:48 -0800

    [Refactor] Remove redundant continue in cc_test_filter_generator.py
    This CL removes a redundant 'continue' statement in 'trim_comments' function.
    The 'continue' statement was at the end of an 'elif' block in a 'while' loop, where the subsequent 'else' block would be skipped anyway by the control flow of 'if/elif/else'. Removing it simplifies the code without changing logic.
    
    Test: atest atest_unittests
    Flag: TEST_ONLY
    OFF_PEAK_PRESUBMIT: true
    
    Change-Id: Ib95e0f1c02929c955dd37be8d772f0b0d00b3bea

--- atest/cc_test_filter_generator.py
+++ atest/cc_test_filter_generator.py
@@ -81,7 +81,6 @@ def trim_comments(content):
     elif comment_type == CCCommentType.LINE_COMMENT:
       trimmed_line = line[:index]
       trimmed_lines.append(trimmed_line.rstrip())
-      continue
     else:
       code_lines = []
       code_line, comment_ended = _handle_block_comment_line(
