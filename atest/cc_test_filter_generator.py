eb289f1ad9f5eb373241bf82c0cd37128dcf18b1
Yuexi Ma <yuexima@google.com>
2025-11-24 10:34:39 -0800

    [Refactor] Simplify slicing in trim_comments
    This change replaces `line[0:index] if index > 0 else ''` with
    `line[:index]`. Python slicing `[:index]` handles `index=0` correctly
    (returning an empty string) and is cleaner and more idiomatic.
    This does not change any logic as `index` is guaranteed to be non-negative.
    
    Test: atest atest_unittests
    Flag: TEST_ONLY
    OFF_PEAK_PRESUBMIT: true
    
    Change-Id: I827c6fca264b802c1a5ed339a58382b945c819aa

--- atest/cc_test_filter_generator.py
+++ atest/cc_test_filter_generator.py
@@ -76,7 +76,7 @@ def trim_comments(content):
     if comment_type == CCCommentType.NO_COMMENT:
       trimmed_lines.append(line.rstrip())
     elif comment_type == CCCommentType.LINE_COMMENT:
-      trimmed_line = line[0:index] if index > 0 else ''
+      trimmed_line = line[:index]
       trimmed_lines.append(trimmed_line.rstrip())
       continue
     else:
