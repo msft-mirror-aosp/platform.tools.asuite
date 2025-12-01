81dc55b4b39772942cf7f3a04a83fbdff104e9e7
Yuexi Ma <yuexima@google.com>
2025-11-30 21:34:07 -0800

    [Refactor] Use set.isdisjoint() in cli_translator.py
    Using `set.isdisjoint(other)` is more semantic and readable than `not set.intersection(other)`. It also avoids creating an intermediate intersection set.
    
    Test: atest atest_unittests
    Flag: TEST_ONLY
    OFF_PEAK_PRESUBMIT: true
    
    Change-Id: Id52ed6e47d8e7bd6240e3a8689b173f5da0412ec

--- atest/cli_translator.py
+++ atest/cli_translator.py
@@ -493,9 +493,9 @@ class CLITranslator:
                 ' occur if the test module is not built for your current lunch'
                 ' target.\n'
             )
-          elif not set(
-              test_mod_info.get('compatibility_suites', [])
-          ).intersection(constants.TEST_MAPPING_SUITES):
+          elif set(test_mod_info.get('compatibility_suites', [])).isdisjoint(
+              constants.TEST_MAPPING_SUITES
+          ):
             print(
                 f'WARNING: Please add {atest_utils.mark_red(test["name"])} to'
                 ' either suite:'
