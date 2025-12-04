1060065f7750a23e39f28ff7e0a403e92f35517a
Yuexi Ma <yuexima@google.com>
2025-12-03 16:32:53 -0800

    [Refactor] Use list comprehension in get_iterations_summary
    Refactor the for loop in get_iterations_summary to use a list comprehension. This improves readability and is more pythonic. The logic remains the same.
    
    Test: atest atest_unittests
    Flag: TEST_ONLY
    OFF_PEAK_PRESUBMIT: true
    
    Change-Id: Ia4deb9ce28211a8afc0713be375ef86ace5a11e1

--- atest/result_reporter.py
+++ atest/result_reporter.py
@@ -329,13 +329,12 @@ class ResultReporter:
 
   def get_iterations_summary(self) -> str:
     """Print the combined summary of all the iterations."""
-    summary_lines = []
-    for key, value in ITER_COUNTS.items():
-      summary_lines.append(
-          f'{key}: Passed: {value.get("passed", 0)}, Failed:'
-          f' {value.get("failed", 0)}, Ignored: {value.get("ignored", 0)},'
-          f' Assumption_failed: {value.get("assumption_failed", 0)}'
-      )
+    summary_lines = [
+        f'{key}: Passed: {value.get("passed", 0)}, Failed:'
+        f' {value.get("failed", 0)}, Ignored: {value.get("ignored", 0)},'
+        f' Assumption_failed: {value.get("assumption_failed", 0)}'
+        for key, value in ITER_COUNTS.items()
+    ]
     total_summary = '\n'.join(summary_lines)
     return f"{au.delimiter('-', 7)}\nITERATIONS RESULT\n{total_summary}\n"
 
