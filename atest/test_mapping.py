cf8c7d2f7403d7a787a486dc2df0a5238a962ebe
Yuexi Ma <yuexima@google.com>
2025-12-05 15:28:41 -0800

    [Refactor] Fix JSON syntax error in TestDetail.__init__ docstring
    Correct the example JSON in the docstring of TestDetail.__init__.
    Previously, the example had an extra comma and closing brace, which made
    it invalid JSON. This change fixes the syntax to improve documentation
    clarity.
    
    Test: atest atest_unittests
    Flag: TEST_ONLY
    OFF_PEAK_PRESUBMIT: true
    
    Change-Id: I4c50b74a009ec0a6a2053bd8ea230d69392b2b6e

--- atest/test_mapping.py
+++ atest/test_mapping.py
@@ -36,7 +36,7 @@ class TestDetail:
         {
           "instrumentation-arg":
               "annotation=android.platform.test.annotations.Presubmit"
-        },
+        }
       ],
       "file_patterns": ["(/|^)Window[^/]*\\.java",
                        "(/|^)Activity[^/]*\\.java"]
