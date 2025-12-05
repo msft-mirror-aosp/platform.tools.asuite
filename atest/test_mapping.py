07146d290090f13ec72a0f448ff5a748a450d25f
Yuexi Ma <yuexima@google.com>
2025-12-05 10:56:54 -0800

    [Refactor] Use f-string in Import.__str__
    This change refactors the Import.__str__ method in test_mapping.py to use an f-string instead of old-style string formatting. This improves readability and consistency with modern Python style without altering the existing logic or behavior of the code. The functionality remains identical.
    
    Test: atest atest_unittests
    Flag: TEST_ONLY
    OFF_PEAK_PRESUBMIT: true
    
    Change-Id: I2177813528bdfe7d6cacbf4f52337a93e2941bac

--- atest/test_mapping.py
+++ atest/test_mapping.py
@@ -65,10 +65,11 @@ class TestDetail:
     host_info = ', runs on host without device required.' if self.host else ''
     if not self.options:
       return self.name + host_info
-    options_str = ', '.join([f'{k}:' if not v else f'{k}: {v}' for k, v in self.options])
+    options_str = ', '.join(
+        [f'{k}:' if not v else f'{k}: {v}' for k, v in self.options]
+    )
 
     return f'{self.name} ({options_str}){host_info}'
-
 
   def __hash__(self):
     """Get the hash of TestDetail based on the details"""
@@ -102,7 +103,7 @@ class Import:
 
   def __str__(self):
     """String value of the Import object."""
-    return 'Source: %s, path: %s' % (self.test_mapping_file, self.path)
+    return f'Source: {self.test_mapping_file}, path: {self.path}'
 
   def get_path(self):
     """Get the path to TEST_MAPPING import directory."""
