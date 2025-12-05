af8abd52adb7c5d8c241834703d9a95780352c06
Yuexi Ma <yuexima@google.com>
2025-12-05 14:10:42 -0800

    [Refactor] Simplify f-string in TestDetail.__str__
    This change simplifies the f-string used to format the options string in the TestDetail.__str__ method. Instead of using a list comprehension within the join method, it directly uses a generator expression.
    
    This improves readability and conciseness without altering the existing logic or functionality of the code.
    
    Test: atest atest_unittests
    Flag: TEST_ONLY
    OFF_PEAK_PRESUBMIT: true
    
    Change-Id: Ifb29329e301b85c1da7d573b49dd495eec0c8758

--- atest/test_mapping.py
+++ atest/test_mapping.py
@@ -66,7 +66,7 @@ class TestDetail:
     if not self.options:
       return f'{self.name}{host_info}'
     options_str = ', '.join(
-        [f'{k}: {v}' if v else f'{k}:' for k, v in self.options]
+        f'{k}: {v}' if v else f'{k}:' for k, v in self.options
     )
 
     return f'{self.name} ({options_str}){host_info}'
